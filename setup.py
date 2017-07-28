e6a8c9ef26d93a52fd8487b60a652d096ec9d42e
Adam Di Carlo <adam@gw-ny.onshored.com>
2017-07-28 01:18:18 -0400

    differentiate my fixes from upstream

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if sys.version_info >= (3,):
 
 setup(
     name = "lcdproc",
-    version = "0.3",
+    version = "0.3.1",
     author = "JingleManSweep",
     author_email = "jinglemansweep@gmail.com",
     description = "Python OOP Wrapper Library for LCDproc Telnet API",
